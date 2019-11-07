3199af255a639e2df5b574ba70db180725761978
rowdyrose <sanders.tessa@gmail.com>
2019-11-06 22:31:44 -0600

    hello world

---
+++ src/main/java/com/tessasanders/dressingselector/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.tessasanders.dressingselector.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/tessasanders/dressingselector/DressingSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tessasanders.dressingselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressingSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
