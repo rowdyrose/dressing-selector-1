1aadc55ae7bc536a497c622a8fdd1005a1941f03
rowdyrose <sanders.tessa@gmail.com>
2019-11-10 13:53:23 -0600

    created dresing title and a short list to test out

---
+++ src/main/java/com/tessasanders/dressingselector/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package com.tessasanders.dressingselector.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        ArrayList<String> dressings = new ArrayList<>();
+        dressings.add("Alginate");
+        dressings.add("ABD Pads");
+        dressings.add("Bulky");
+
+        model.addAttribute("dressings", dressings);
+        model.addAttribute("title", "Dressing Selector");
+        return "index";
+    }
+
+    @RequestMapping(value = "search")
+    public String DisplaySearchForm(Model model) {
+        model.addAttribute("title", "Search Dressings")
+    }
+
+
+}
