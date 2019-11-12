8169833ec3bcb40a94cac59fee90167d38809a81
rowdyrose <sanders.tessa@gmail.com>
2019-11-11 20:50:08 -0600

    attempting to add a search option

--- src/main/java/com/tessasanders/dressingselector/controllers/HomeController.java
+++ src/main/java/com/tessasanders/dressingselector/controllers/HomeController.java
@@ -4,7 +4,10 @@ package com.tessasanders.dressingselector.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Controller
@@ -13,20 +16,23 @@ public class HomeController {
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        ArrayList<String> dressings = new ArrayList<>();
-        dressings.add("Alginate");
-        dressings.add("ABD Pads");
-        dressings.add("Bulky");
-
-        model.addAttribute("dressings", dressings);
+        model.addAttribute("dressings", "dressings");
         model.addAttribute("title", "Dressing Selector");
         return "index";
     }
 
-    @RequestMapping(value = "search")
-    public String DisplaySearchForm(Model model) {
-        model.addAttribute("title", "Search Dressings");
-        return "results";
+    @RequestMapping(value = "search", method = RequestMethod.GET)
+    public String DisplaySearchForm(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    @RequestMapping(value = "search", method = RequestMethod.POST)
+    public String processSearchForm() {
+
+
     }
 
 
